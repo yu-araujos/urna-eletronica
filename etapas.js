9810a57087669027c0db0f63bcbc409391d17b80
Yuri <yuri.araujo.silva@gmail.com>
2021-08-04 19:32:04 -0300

    Adição do projeto Urna Eletrônica com a linguagem de Javascript

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+      titulo: 'VEREADOR',
+      numeros: 5,
+      candidatos: [
+          {
+              numero: '38111',
+              nome: 'José Bezerra',
+              partido: 'PTM',
+              fotos: [
+                  {url: '38111.jpg', legenda: 'Vereador'}
+              ]
+          },
+          {
+              numero: '77222',
+              nome: 'Luna Silva',
+              partido: 'PBTH',
+              fotos: [
+                  {url: '77222.jpg',legenda: 'Vereador'}
+              ]
+          }
+      ]
+  },
+  {
+      titulo: 'PREFEITO',
+      numeros: 2,
+      candidatos: [
+          {
+              numero: '84',
+              nome: 'Woody',
+              partido: 'PTSD',
+              vice: 'Buzz',
+              fotos: [
+                  {url: '84.jpg', legenda: 'Prefeito'},
+                  {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+              ]
+          },
+          {
+              numero: '99',
+              nome: 'Jessie',
+              partido: 'PAJCW',
+              vice: 'Bob',
+              fotos: [
+                  {url: '99.jpg', legenda: 'Prefeito'},
+                  {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+              ]
+          }
+      ]
+  }
+];
